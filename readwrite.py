d6b3d730929f6b0477833ea12af4d7bf9edd88a1
Ashlyn Kong <kong.ashlyn@gmail.com>
2019-01-14 17:11:46 -0500

    simplify code in readwrite.py

--- readwrite.py
+++ readwrite.py
@@ -5,7 +5,5 @@ print(full_text)
 
 # Writing a text file
 with open('hello.txt', 'w') as f:
-    f.write('Hello, my name is ')
-    f.write(full_text)
-    f.write('.')
+    f.write('Hello, my name is ' + full_text + '.')
     f.write('\n')

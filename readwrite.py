a0d8a5dd277c40de487287c2c7cfdd4a0e37c921
Ashlyn Kong <kong.ashlyn@gmail.com>
2019-01-14 17:05:33 -0500

    add read write python examples

---
+++ readwrite.py
@@ -0,0 +1,10 @@
+with open('name.txt') as f:
+    full_text = f.read()
+
+print(full_text)
+
+# Writing a text file
+with open('hello.txt', 'w') as f:
+    f.write('Hello, my name is ')
+    f.write(full_text)
+    f.write('\n')
